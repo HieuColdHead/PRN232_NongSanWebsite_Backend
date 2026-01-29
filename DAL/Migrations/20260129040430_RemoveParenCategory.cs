14fb89e12d60aff12638c9ef50ea80c628b06dec
Nhoczip0904 <nguyenhungthinh04@gmail.com>
2026-01-29 11:16:13 +0700

    migration

---
+++ DAL/Migrations/20260129040430_RemoveParenCategory.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveParenCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Categories_parent_category_id",
+                table: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_parent_category_id",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "parent_category_id",
+                table: "Categories");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "parent_category_id",
+                table: "Categories",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_parent_category_id",
+                table: "Categories",
+                column: "parent_category_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Categories_parent_category_id",
+                table: "Categories",
+                column: "parent_category_id",
+                principalTable: "Categories",
+                principalColumn: "category_id");
+        }
+    }
+}
